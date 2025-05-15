5a829f3ebd73a117c225fdd5ff3108832d9e681e
Élcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-05-14 22:43:44 -0300

    feat: Criação de uma rota para logs e novo parâmetro que permite definir qual é o tipo de retorno da chamada API através da query.

--- src/controllers/container.ts
+++ src/controllers/container.ts
@@ -1,17 +1,66 @@
 import { docker } from "../core/docker";
 import { format } from "../utils/response";
+import { demultiplex } from "../utils/demultiplex";
+import { default_view } from "../views/default";
 
-export async function containers() {
+export async function containers_controller(view: string = "default") {
   try {
     const containers = await docker.listContainers();
 
-    return format(containers);
+    switch (view) {
+      case "raw":
+        return format(containers);
+      default:
+        return format(default_view(containers));
+    }
   } catch (error) {
     return format(null, "fail", error);
   }
 }
 
-export async function container(id: string) {
+export async function container_controller(
+  id: string,
+  view: string = "default"
+) {
   try {
-  } catch (error) {}
+    const container = await docker.getContainer(id).inspect();
+
+    switch (view) {
+      case "raw":
+        return format(container);
+      default:
+        return format(default_view(container));
+    }
+  } catch (error) {
+    return format(null, "fail", error);
+  }
+}
+
+export async function logs_controller(id: string) {
+  try {
+    const container = docker.getContainer(id);
+
+    const logs: Buffer = await new Promise((resolve, reject) => {
+      container.logs(
+        {
+          stdout: true,
+          stderr: true,
+          follow: false,
+          tail: 100,
+          timestamps: true,
+        },
+        (err, data?: Buffer) => {
+          if (err) return reject(err);
+          if (!data) return resolve(Buffer.from(""));
+          resolve(data);
+        }
+      );
+    });
+
+    const { stdout, stderr } = demultiplex(logs);
+
+    return format({ stdout, stderr });
+  } catch (error) {
+    return format(null, "fail", error);
+  }
 }
