ce5a711eea6579405eac766acf7ed2844bf2901a
Élcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-05-14 22:44:18 -0300

    feat: Refatoração das rotas de containers para incluir middleware de log e adicionar a passagem de parâmetro por query

--- src/routes/containers.ts
+++ src/routes/containers.ts
@@ -1,16 +1,30 @@
 import express from "express";
-import { container, containers } from "../controllers/container";
+import {
+  container_controller,
+  containers_controller,
+  logs_controller,
+} from "../controllers/container";
+import { log_middleware } from "../middlewares/log";
 
-export const router = express.Router();
+export const container_router = express.Router();
 
-router.get("/", async (req, res) => {
-  const ret = await containers();
+container_router.get("/", log_middleware, async (req, res) => {
+  const view = req.query.view as string;
 
-  res.json(ret);
+  res.json(await containers_controller(view));
 });
 
-router.get("/:id", async (req, res) => {
+container_router.get("/:id", log_middleware, async (req, res) => {
+  const { id } = req.params;
+  const view = req.query.view as string;
+
+  res.json(await container_controller(id, view));
+});
+
+container_router.get("/:id/logs", log_middleware, async (req, res) => {
   const { id } = req.params;
 
-  const ret = await container(id);
+  const ret = await logs_controller(id);
+
+  res.json(ret);
 });
